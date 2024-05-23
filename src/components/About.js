fa17215269cafb04a4ad1155d4a4040843898f90
Elijah Alaak <alaakelijah78@gmail.com>
2024-05-23 07:26:11 +0300

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,14 @@ import { image } from "../data/data";
 
 function About() {
   return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>123</p>
+      <img src={image} alt=" I made this"/>
+
+    </div>
+  );
 }
 
 export default About;
